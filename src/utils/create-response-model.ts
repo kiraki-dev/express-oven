2aa5d97599aa5a405437f68fa634caa81f92ca67
MyasnikKhachkalyan <myasnik@kiraki.dev>
2021-09-20 18:10:26 +0400

    [fix] response model

--- src/utils/create-response-model.ts
+++ src/utils/create-response-model.ts
@@ -12,7 +12,7 @@ export const createResponseBuilder = (responseModel: ResponseModel): ResponseMod
 
   return {
     setData(data: any): void {
-      response = sepPropDeep(responseModel, responseModel.paths.data, data);
+      response = sepPropDeep(response, responseModel.paths.data, data);
     },
     write(res: Response) {
       response === undefined ? res.end() : res.send(response);
